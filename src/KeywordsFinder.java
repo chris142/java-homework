4899140ab1ad1c700be38fbcbe275882a5497b79
Kristina Zuenok <kris142@mail.ru>
2017-10-05 07:07:12 +0300

    task #5 - done

--- src/KeywordsFinder.java
+++ src/KeywordsFinder.java
@@ -4,60 +4,44 @@ import java.nio.file.attribute.BasicFileAttributes;
 
 class MyFileFindVisitor extends SimpleFileVisitor<Path> {
 
-    private PathMatcher matcher;
+    private String keyWordFirst = "transient";
+    private String keyWordSecond = "volatile";
 
-    public MyFileFindVisitor(String pattern) {
-        try {
-            matcher = FileSystems.getDefault().getPathMatcher(pattern);
-        } catch (IllegalArgumentException iae) {
-            System.err
-                    .println("Invalid pattern");
-            System.exit(1);
-        }
-    }
-
-    public FileVisitResult visitFile(Path path,
-                                     BasicFileAttributes fileAttributes) {
-        find(path);
+    public FileVisitResult visitFile(Path path, BasicFileAttributes fileAttributes) {
 
 
+        if (path.getFileName().toString().contains(".java"))
+            try {
+                String content = new String(Files.readAllBytes(path));
 
+                boolean containsFirst = false;
+                if (keyWordFirst != null && content.contains(keyWordFirst))
+                    containsFirst = true;
 
+                boolean containsSecond = false;
+                if (keyWordSecond != null && content.contains(keyWordSecond))
+                    containsSecond = true;
 
+                if (containsFirst && containsSecond)
+                    System.out.println(path);
 
-        return FileVisitResult.CONTINUE;
-    }
+            } catch (IOException e) {
+                System.out.println(e);
+            }
 
-
-
-    private void find(Path path) {
-        Path name = path.getFileName();
-        if (matcher.matches(name))
-            System.out.println("Matching file: " + path.getFileName());
-    }
-
-
-
-
-    public FileVisitResult preVisitDirectory(Path path,
-                                             BasicFileAttributes fileAttributes) {
-        find(path);
         return FileVisitResult.CONTINUE;
     }
 }
 
 
-
 public class KeywordsFinder {
     public static void main(String[] args) {
-        Path startPath = Paths.get("/Users/Van-ess0/IdeaProjects/java-homework/aaa");
+        Path startPath = Paths.get("/Users/Van-ess0/IdeaProjects/java-homework/aaa/src"); //ToChange
 
-        //Строка с glob-шаблоном  
-        String pattern = "glob:*volatile*";
 
         try {
-            Files.walkFileTree(startPath, new MyFileFindVisitor(pattern));
-            System.out.println("Done!");
+            Files.walkFileTree(startPath, new MyFileFindVisitor());
+            System.out.println("Checked!");
         } catch (IOException e) {
             e.printStackTrace();
         }
