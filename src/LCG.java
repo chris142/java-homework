405f3ddde2e5deb50765199ebc5181ad3669549d
Kristina Zuenok <kris142@mail.ru>
2017-10-07 04:35:30 +0300

    task #8 - done

---
+++ src/LCG.java
@@ -0,0 +1,18 @@
+
+class LCG {
+
+    private long a; // multiplier
+    private long c; // increment
+    private long m; // modulus
+
+    LCG(long a, long c, long m) {
+
+        this.a = a;
+        this.c = c;
+        this.m = m;
+    }
+
+    long getNext(long prev) {
+        return (this.a * prev + this.c) % this.m;
+    }
+}
---
+++ src/MainRandNumGenerator.java
@@ -0,0 +1,18 @@
+
+import java.util.stream.Stream;
+
+public class MainRandNumGenerator {
+
+
+    public static void main(String[] args) {
+
+        final long l2pow48 = 281474976710656L;
+
+        final LCG random = new LCG(25214903917L, 11L, l2pow48);
+
+        Stream<Long> stream = Stream.iterate(17L, random::getNext).limit(10);
+
+        stream.forEach(System.out::println);
+
+    }
+}
